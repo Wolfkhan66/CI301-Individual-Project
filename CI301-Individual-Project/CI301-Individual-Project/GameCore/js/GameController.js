cc4ee713c060005e32f7432f8f0be5f298c4f240
Cai Lehwald <cailehwald@googlemail.com>
2018-03-06 13:17:31 +0000

    Add Game window, tweak index page and layout.cshtml. cleanup and implement a UI class

---
+++ CI301-Individual-Project/CI301-Individual-Project/GameCore/js/GameController.js
@@ -0,0 +1,32 @@
+﻿function main() {
+    console.log("main();");
+
+    const GAMEWIDTH = 800;
+    const GAMEHEIGHT = 600;
+
+    // Initialize the phaser game window, give it a width of GAMEWIDTH and a height of GAMEHEIGHT, set the rendering context to auto and attach the window to a div with the ID "GameWindow"
+    game = new Phaser.Game(GAMEWIDTH, GAMEHEIGHT, Phaser.AUTO, 'GameWindow', {
+        preload: preload,
+        create: create,
+        update: update
+    });
+}
+
+function preload() {
+    console.log("Loading Assets...");
+    // Load game assets \\
+    console.log("Assets Loaded.");
+}
+
+
+function create() {
+    console.log("Creating World...");
+    // Load System Classes \\
+    ui = new UI();
+
+    console.log("Creation complete.");
+}
+
+function update() {
+
+}
---
+++ CI301-Individual-Project/CI301-Individual-Project/GameCore/js/UI.js
@@ -0,0 +1,79 @@
+﻿class UI {
+    // This UI class is based on the https://github.com/Wolfkhan66/Phaser-First-Game-Tutorial repository in which i designed a system class to allow the dynamic creation of text and sprites as UI elements.
+    // The code has been remodified and updated for this project.
+    constructor() {
+        console.log("Constructing UI Elements")
+        this.elements = [];
+        
+        //MainMenu UI
+        
+        //AISelect UI
+
+        //InGame UI
+    }
+
+    setScreen(screen) {
+        hideAll();
+        switch (screen) {
+            case "MainMenu":
+                showUI("MainMenu");
+                break;
+            case "AISelect":
+                showUI("AISelect");
+                break;
+            case "InGame":
+                showUI("InGame");
+                break;
+            default:
+                console.log(screen + " not found");
+        }
+    }
+
+    createText(name, UI, x, y, string, size) {
+        var textObject = game.add.text(0, 0, string, {
+            font: size + 'px Old English Text MT',
+            fill: '#fff'
+        });
+        this.elements.push({ Name: name, UI: UI, Type:"Text", Object: textObject });
+    }
+
+    createSprite(name, UI, x, y, width, height, image) {
+        var sprite = game.add.sprite(0, 0, image);
+        sprite.width = width;
+        sprite.height = height;
+        this.elements.push({ Name: name, UI: UI, Type:"Sprite", Object: sprite });
+    }
+
+    addEventListener(name, eventDown, eventUp) {
+        this.elements.forEach(function (object) {
+            if (object.Name == name) {
+                if (eventDown != null) {
+                    object.inputEnabled = true;
+                    object.events.onInputDown.add(eventDown, this);
+                }
+                if (eventUp != null) {
+                    object.inputEnabled = true;
+                    object.events.onInputUp.add(eventUp, this);
+                }
+            }
+        });
+    }
+
+    showUI(UIType) {
+        this.elements.forEach(function (object) {
+            if (object.UI == UIType) {
+                if (object.type == "Text") { object.Text.visible = true; }
+                if (object.type == "Sprite") { object.Sprite.visible = true;  }
+            }
+        });
+    }
+
+    hideAll() {
+        this.elements.forEach(function (object) {
+            if (object.UI == UIType) {
+                if (object.type == "Text") { object.Text.visible = false; }
+                if (object.type == "Sprite") { object.Sprite.visible = false; }
+            }
+        });
+    }
+}
